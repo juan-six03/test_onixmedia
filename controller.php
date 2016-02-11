0d81be5ff1d7d38641459a2c2476ed00669b70ec
Guest User <juan.six03@gmail.com>
2016-02-11 13:38:12 -0600

    ...

--- controller.php
+++ controller.php
@@ -1,50 +1,53 @@
-<?php
-
 class basicOperation {
 
-	public function __constructor(){
-	
-	}
+        public function __constructor(){
 
-	public function suma($array_numbers){
-		$res = 0;
-		foreach($array_numbers as $ele){
-			$res += $ele;
+        }
+
+        public function suma($array_numbers){
+                $res = 0;
+                foreach($array_numbers as $ele){
+                	if(is_numeric($ele)){
+                        $res += $ele;
+					}else{
+						throw Exception("Imposible realizar suma, {$ele} no es un numero.");
+					}
+                }
+                echo "La suma de los numeros introducidos, es: {$res}";
+        }
+        public function resta($array_numbers){
+        	 foreach($array_numbers as $ele){
+                	if(!is_numeric($ele)){
+						throw Exception("Imposible realizar resta, {$ele} no es un numero.");
+					}
+             }
+             $res = $array_numbers["num1"] - $array_numbers["num2"];
+        	echo "La resta de los numeros introducidos es: {$res}";
 		}
-		return $res;
-	}
-	public function resta($array_numbers){
-                //$res = 0;
-                //foreach($array_numbers as $ele){
-                //        $res -= $ele;
-                //}
-                return $array_numbers["num1"] - $array_numbers["num2"];
-        }	        
-	public function producto($array_numbers){
-//                $res = 0;
-//                foreach($array_numbers as $ele){
-//                        $res *= $ele;
-//                }
-                return $array_numbers["num1"] * $array_numbers["num2"];
-        }
-	public function division($array_numbers){
-                //$res = 0;
-                //foreach($array_numbers as $ele){
-                //        $res /= $ele;
-                //}
-                return $array_numbers["num1"] / $array_numbers["num2"];
-        }
-                
+        public function producto($array_numbers){
+        	foreach($array_numbers as $ele){
+                	if(!is_numeric($ele)){
+						throw Exception("Imposible realizar multiplicacion, {$ele} no es un numero.");
+					}
+             }
+             $res =  $array_numbers["num1"] * $array_numbers["num2"];
+        	echo "La resta de los numeros introducidos es: {$res}";
+		}
+        public function division($array_numbers){
+        		$res = 0;
+        	foreach($array_numbers as $ele){
+                	if(!is_numeric($ele)){
+						throw Exception("Imposible realizar division, {$ele} no es un numero.");
+					}
+             }
+			
+			try{
+				$res = $array_numbers["num1"] / $array_numbers["num2"];
+        	}catch(Exception $e){
+        		echo "No es posible realizar division entre 0";
+        	}
+        	echo "La division de los numeros introducidos es:{$res}";
+		}
+
 }
-$numeros = [
-"num1" => 10,
-"num2" => 15
-];
 
-$op = new basicOperation();
-$suma = $op->suma($numeros);
-$resta = $op->resta($numeros);
-$producto = $op->producto($numeros);
-$division = $op->division($numeros);
-
-?>
